ece204ecdebe825b55296fc9a31803829308e76e
Renzo Zukeram <renzo.zuk19@gmail.com>
2025-10-19 02:25:07 -0300

    Feat: PromptController

---
+++ src/main/java/dev/renzozukeram/recommendation/controllers/PromptController.java
@@ -0,0 +1,21 @@
+package dev.renzozukeram.recommendation.controllers;
+
+import dev.renzozukeram.recommendation.dto.PromptRequest;
+import dev.renzozukeram.recommendation.services.ChatService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/chat")
+public class PromptController {
+
+    @Autowired
+    private ChatService chatService;
+
+
+    @PostMapping
+    public ResponseEntity<String> prompt(@RequestBody PromptRequest promptRequest) {
+        return ResponseEntity.ok(chatService.getAnswer(promptRequest));
+    }
+}
