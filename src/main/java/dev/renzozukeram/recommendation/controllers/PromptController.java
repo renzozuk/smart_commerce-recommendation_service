c5cebd0f60a5161276fa1dacac3aa3b345ba1b71
Renzo Zukeram <renzo.zuk19@gmail.com>
2025-10-28 02:10:45 -0300

    wip: Adjust for graphql

--- src/main/java/dev/renzozukeram/recommendation/controllers/PromptController.java
+++ src/main/java/dev/renzozukeram/recommendation/controllers/PromptController.java
@@ -2,21 +2,34 @@ package dev.renzozukeram.recommendation.controllers;
 
 import dev.renzozukeram.recommendation.services.ChatService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.MutationMapping;
+import org.springframework.stereotype.Controller;
 
 import java.util.List;
-import java.util.Map;
 
-@RestController
-@RequestMapping("/chat")
+@Controller
 public class PromptController {
 
     @Autowired
     private ChatService chatService;
 
-    @PostMapping
-    public ResponseEntity<List<String>> prompt(@RequestBody Map<String, Object> requestBody) {
-        return ResponseEntity.ok(chatService.getAnswer(requestBody.get("userInfo"), requestBody.get("userViews"), requestBody.get("userPurchases"), requestBody.get("userReturns"), requestBody.get("products")));
+    public record PromptInput(
+            String userInfo,
+            String userViews,
+            String userPurchases,
+            String userReturns,
+            String products
+    ) {}
+
+    @MutationMapping
+    public List<String> prompt(@Argument PromptInput input) {
+        return chatService.getAnswer(
+                input.userInfo(),
+                input.userViews(),
+                input.userPurchases(),
+                input.userReturns(),
+                input.products()
+        );
     }
 }
