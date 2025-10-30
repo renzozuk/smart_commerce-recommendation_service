6980817baf0457dc97424795627ee8e66572a49f
Renzo Zukeram <renzo.zuk19@gmail.com>
2025-10-30 01:12:27 -0300

    Chore: Adjust for graphql

--- src/main/java/dev/renzozukeram/recommendation/controllers/PromptController.java
+++ src/main/java/dev/renzozukeram/recommendation/controllers/PromptController.java
@@ -2,21 +2,35 @@ package dev.renzozukeram.recommendation.controllers;
 
 import dev.renzozukeram.recommendation.services.ChatService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.MutationMapping;
+import org.springframework.stereotype.Controller;
 import reactor.core.publisher.Flux;
 
-import java.util.Map;
 import java.util.UUID;
 
-@RestController
-@RequestMapping("/chat")
+@Controller
 public class PromptController {
 
     @Autowired
     private ChatService chatService;
 
-    @PostMapping
-    public Flux<UUID> prompt(@RequestBody Map<String, Object> requestBody) {
-        return chatService.getAnswer(requestBody.get("userInfo"), requestBody.get("userViews"), requestBody.get("userPurchases"), requestBody.get("userReturns"), requestBody.get("products"));
+    public record PromptInput(
+            String userInfo,
+            String userViews,
+            String userPurchases,
+            String userReturns,
+            String products
+    ) {}
+
+    @MutationMapping
+    public Flux<UUID> prompt(@Argument PromptInput input) {
+        return chatService.getAnswer(
+                input.userInfo(),
+                input.userViews(),
+                input.userPurchases(),
+                input.userReturns(),
+                input.products()
+        );
     }
 }
